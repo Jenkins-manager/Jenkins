d18822ff5fd5e014938494ea18872ff7afab4328
Jack Branch <39771802+pliantmeerkat@users.noreply.github.com>
2018-09-17 17:01:57 +0100

    adding default timestamp to model

--- jenkinsServer/statement/models.py
+++ jenkinsServer/statement/models.py
@@ -2,15 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class Question(models.Model):
     id = models.IntegerField(primary_key=True, null=False)
     body = models.CharField(max_length=50, null=False)
-    created_at = models.DateTimeField(null=False)
+    created_at = models.DateTimeField(default=datetime.now, blank=True, null=False)
 
 class Answer(models.Model):
     id = models.IntegerField(primary_key=True, null=False)
     body = models.CharField(max_length=50, null=False)
-    created_at = models.DateTimeField(null=False)
+    created_at = models.DateTimeField(default=datetime.now, blank=True, null=False)
