00241b05572d5f680e3ff8cfd692e4515d8efc18
Daniel den Hartog <daniel.mista@gmail.com>
2018-09-20 17:04:02 +0100

    unit tests for functions inside answer table

--- Server/model/answer_processor.py
+++ Server/model/answer_processor.py
@@ -2,10 +2,22 @@
     Answer Processing class - calls a function then returns to request_processor
 """
 
-import datetime
+from time import gmtime, strftime
 
 class AnswerProcessor:
 
     @staticmethod
     def getTime():
-        print(datetime.datetime.now().time())
+        return strftime("%H:%M:%S", gmtime())
+
+    @staticmethod
+    def getName():
+        return 0
+
+    @staticmethod
+    def getDate():
+        return strftime("%Y-%m-%d", gmtime())
+
+    @staticmethod
+    def getWeather():
+        return 0
--- Server/tests/test_answer_processor.py
+++ Server/tests/test_answer_processor.py
@@ -3,3 +3,12 @@
 """
 import pytest
 from ..model.answer_processor import AnswerProcessor
+from time import gmtime, strftime
+
+class TestClass(object):
+
+    def test_getTime(self):
+        assert AnswerProcessor.getTime() == strftime("%H:%M:%S", gmtime())
+
+    def test_getDate(self):
+        assert AnswerProcessor.getDate() == strftime("%Y-%m-%d", gmtime())
