98596a3cf2d64b75ff088d4d007d3fa9f34eae2f
Jack Branch <39771802+pliantmeerkat@users.noreply.github.com>
2018-09-24 11:15:46 +0100

    added test functions

--- Server/model/machine_learning/machine_learn.py
+++ Server/model/machine_learning/machine_learn.py
@@ -5,4 +5,21 @@
 class MachineLearn:
 
     def __init__(self):
-        print("creatd")
+        print("created")
+
+    def normalize(self, address):
+        print("normalize")
+    
+    def de_normalize(self, address):
+        print("denormalize")
+    
+    def train_network(self, value_set):
+        print("train!")
+
+    def get_output(self, value_set):
+        print("output")
+    
+    def verify_output(self, output):
+        print("verified")
+    
+    # run?
--- Server/tests/machine_learning_tests/test_machine_learn.py
+++ Server/tests/machine_learning_tests/test_machine_learn.py
@@ -6,5 +6,17 @@ from ...model.machine_learning.machine_learn import MachineLearn
 
 class TestClass(object):
 
-    def test_1(self):
+    def test_normalize(self):
+        assert True
+    
+    def test_de_normalize(self):
+        assert True
+
+    def test_train_network(self):
+        assert True
+
+    def test_get_output(self):
+        assert True
+
+    def test_verify_output(self):
         assert True
