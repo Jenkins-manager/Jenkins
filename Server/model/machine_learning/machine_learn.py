75cec5f4d4a32718c3a943187da4df2b68f9627f
Jack Branch <39771802+pliantmeerkat@users.noreply.github.com>
2018-09-26 12:28:23 +0100

    file io basic operations written and tests passing

--- Server/model/machine_learning/machine_learn.py
+++ Server/model/machine_learning/machine_learn.py
@@ -5,7 +5,7 @@
 import tensorflow as tf
 from tensorflow import keras
 import threading
-from ...model.machine_learning.training_model import TrainingModel
+from training_model import TrainingModel
 import tensorflow as tf
 import numpy
 # import matplotlib.pyplot as plt
