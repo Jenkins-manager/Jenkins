51ce333e8e396f4219ee8c40058cfbaf498ae51e
Daniel den Hartog <daniel.mista@gmail.com>
2018-09-26 11:52:26 +0100

    fixed failing test in test_request_processor

--- Server/tests/test_request_processor.py
+++ Server/tests/test_request_processor.py
@@ -13,20 +13,20 @@ class TestClass(object):
     #     assert RequestProcessor.get_questions(QuestionSerializer, Question).data[0]['body'] == 'What time is it?'
 
     def test_check_request_no_throw_with_vaid_input(self):
-        assert RequestProcessor.check_request({'body': 'What time is it?'}, Question)[0] == True 
+        assert RequestProcessor.check_request({'body': 'What time is it?'}, Question)[0] == True
 
     def test_check_request_returns_correct_address(self):
-        assert RequestProcessor.check_request({'body': "what is today's date?"}, Question)[1] == 2 
+        assert RequestProcessor.check_request({'body': "what is today's date?"}, Question)[1] == 2
 
     def test_check_request_throws_with_no_input(self):
-        assert RequestProcessor.check_request({'body': ''}, Question) == False 
+        assert RequestProcessor.check_request({'body': ''}, Question) == False
 
     def test_check_request_throws_with_invalid_question(self):
-        assert RequestProcessor.check_request({'body': 'fake question'}, Question) == False 
-    
+        assert RequestProcessor.check_request({'body': 'fake question'}, Question) == False
+
     def test_get_answer(self):
         assert RequestProcessor.get_answer(4, Answer) == "The time is: " + strftime("%H:%M:%S", gmtime())
-    
+
     # exception throwing tests
 
     def test_process_request_bad_question_address(self):
@@ -41,4 +41,4 @@ class TestClass(object):
             RequestProcessor.get_answer(400, Answer)
             fail("no exception thrown")
         except Exception, e:
-            assert e == 'Answer matching query does not exist.'
+            assert str(e) == 'Answer matching query does not exist.'
